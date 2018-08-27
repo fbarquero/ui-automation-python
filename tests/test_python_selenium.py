8ba8870f1d80f27d493084eb90200bb557d29494
Francisco Barquero <fbarquero@growthaccelerationpartners.com>
2018-08-27 03:42:31 -0600

    adding changes

--- tests/test_python_selenium.py
+++ tests/test_python_selenium.py
@@ -3,14 +3,11 @@ from unittest import TestCase
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from nose.plugins.attrib import attr
 
 
+@attr('all')
 class TestPythonOrgSearch(TestCase):
-
-    def setUp(self):
-        self.driver = webdriver.Remote(
-            command_executor='http://127.0.0.1:4444/wd/hub',
-            desired_capabilities=DesiredCapabilities.CHROME)
 
     def test_search_in_python_org(self):
         driver = self.driver
@@ -21,6 +18,11 @@ class TestPythonOrgSearch(TestCase):
         elem.send_keys(Keys.RETURN)
         driver.get_screenshot_as_file("screenshot.png")
         assert "No results found." not in driver.page_source
+
+    def setUp(self):
+        self.driver = webdriver.Remote(
+            command_executor='http://127.0.0.1:4444/wd/hub',
+            desired_capabilities=DesiredCapabilities.CHROME)
 
     def tearDown(self):
         self.driver.quit()
